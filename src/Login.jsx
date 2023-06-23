22de0ae465c71c133c3e1c4a2bc691dd2edff9bf
MafNik <marijana.nikacevic99@gmail.com>
2023-06-23 20:24:48 +0200

    kreirana komponenta za login na frontu

---
+++ src/Login.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    axios
+      .post('/login', formData)
+      .then(response => {
+        alert("USPEH")
+        console.log(response.data);
+      })
+      .catch(error => {
+        alert("GRESKA")
+        console.log(error.response.data);
+      });
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
