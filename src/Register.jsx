35fdaa6d559f9c7926f938cf5d62f48294537923
MafNik <marijana.nikacevic99@gmail.com>
2023-06-24 22:34:55 +0200

    dodata komponenta za registracionu formu

---
+++ src/Register.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const { name, email, password } = formData;
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    axios
+      .post('/register', formData)
+      .then(response => {
+        alert("USPEH")
+        console.log(response.data);
+      })
+      .catch(error => {
+        alert("GRESKA")
+        console.log(error.response.data);
+      });
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
