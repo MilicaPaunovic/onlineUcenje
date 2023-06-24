e723875a66c39529faa89854edd4280a459396de
MafNik <marijana.nikacevic99@gmail.com>
2023-06-24 22:36:09 +0200

    kreirane rute

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@
 import './App.css';
 import Login from './Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './Register';
 function App() {
   return (
-    <div className="App">
-      
-      <Login></Login>
-    </div>
+    <Router>
+    <div>
+        <Navbar />
+        <Routes>
+         
+          
+          <Route path="/login" element={   <Login></Login>} />
+          <Route path="/register" element={   <Register></Register>} />
+
+          
+        </Routes>
+        </div>
+    </Router>
+ 
   );
 }
 
